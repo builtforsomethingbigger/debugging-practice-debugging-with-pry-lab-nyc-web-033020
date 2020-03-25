bf8a94bbc02a4fc2030a1d08adc4a5cb1783ef48
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-25 00:48:39 +0000

    Automatically backed up by Learn

--- fix_using_tests/the_final_frontier.rb
+++ fix_using_tests/the_final_frontier.rb
@@ -10,11 +10,11 @@ def state_log(star_date)
 end
 
 def crew
-  ["Geordi", "Data", "Worf", "William", "Beverly", "Deanna"]
+  ["Hello Geordi", "Hello Data", "Hello Worf", "Hello William", "Hello Beverly", "Hello Deanna"]
 end
 
 def greet_crew(crew)
-  crew.each {|crew_member| puts "Hello #{crew[crew_member]}."}
+  crew.each {|crew_member| puts "Hello #{crew_member}."}
 end
 
 def engage
