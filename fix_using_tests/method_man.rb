fc4a5091ffdff93df34d394b843347a83cb642dc
Stephen Cases <builtforsomethingbigger@gmail.com>
2020-03-25 00:17:41 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -1,9 +1,10 @@
 require 'pry'
 
 def start_game(player1, player2)
-  puts "Hello #{player1} & #{playar2}"
+  "Hello #{player1} & #{playar2}"
+  binding.pry
 end
 
-def play_game(player1, player2)
-  puts"#{player1} is better than #{player2}"
+def play_game(player2)
+  "#{player1} is better than #{player2}"
 end
